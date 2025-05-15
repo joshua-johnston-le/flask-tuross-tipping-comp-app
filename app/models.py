e03650e739bfa48900f5262f46d0294f6514749e
joshua-johnston-le <joshua.johnston@luxuryescapes.com>
2025-05-15 12:56:06 +1000

    tipoq

--- app/models.py
+++ app/models.py
@@ -24,7 +24,7 @@ class Tip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     match = db.Column(db.String(100), nullable=False)
     selected_team = db.Column(db.String(100), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     username = db.Column(db.String(150), nullable=False)
     date = db.Column(db.DateTime, default=lambda: datetime.now(au_tz))
     
