b512722b4cd62bc9c60a712fcee921053322ae8b
joshua-johnston-le <joshua.johnston@luxuryescapes.com>
2025-05-15 15:00:27 +1000

    add helper to main route

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -2,9 +2,10 @@
 
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, logout_user, current_user
-from app.models import Tip, FixtureFree
+from app.models import Tip, FixtureFree, UserTipStats
 from app.services.fixtures import find_current_round
 from app.utils.team_logos import TEAM_LOGOS
+from app.utils.helper_functions import get_user_rank
 from datetime import datetime
 
 main_bp = Blueprint('main', __name__)
@@ -20,7 +21,8 @@ def home():
         tips = Tip.query.filter(Tip.user_id==current_user.id, Tip.match.in_(match_ids)).all()
     tip_map = {tip.match: tip.selected_team for tip in tips}
     result_map = {tip.match: FixtureFree.get_winning_team(tip.match) for tip in tips}
-    return render_template('home.html', tips=tips, tip_map=tip_map, result_map=result_map, round_number=round_number, fixtures=fixtures, team_logos=TEAM_LOGOS, current_year=datetime.now().year)
+    rank = get_user_rank(current_user.user_id)
+    return render_template('home.html', tips=tips, tip_map=tip_map, result_map=result_map, round_number=round_number, fixtures=fixtures, team_logos=TEAM_LOGOS, current_year=datetime.now().year, rank=rank)
 
 @main_bp.route('/logout')
 @login_required
