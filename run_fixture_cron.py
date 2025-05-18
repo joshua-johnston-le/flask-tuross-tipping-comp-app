b1bb899cd607a2ce60af071a910ba2231442215a
joshua-johnston-le <joshua.johnston@luxuryescapes.com>
2025-05-18 20:00:04 +1000

    added new prints for monitoring

--- run_fixture_cron.py
+++ run_fixture_cron.py
@@ -1,9 +1,11 @@
 # run_fixtures_cron.py
 
 from app import create_app, db
-from app.services.fixtures import upsert_free_fixtures, update_user_tip_stats
+from app.services.fixtures import upsert_free_fixtures, update_user_tip_stats, find_current_round, get_free_nrl_fixtures
 from datetime import datetime
 import pytz
+from app.models import FixtureFree, User, UserTipStats
+import json
 
 au_datetime = datetime.now(pytz.timezone("Australia/Sydney"))
 
@@ -13,10 +15,21 @@ def run():
         print(f"Initialising Cron Job at: {au_datetime}")
         print("Running cron job: upserting NRL fixtures...")
         upsert_free_fixtures()
-        print("Done.")
+        print("Fixtures updated, ✅.")
+        print("Latest scores updated")
+        fixtures = FixtureFree.query.filter_by(round=find_current_round()).all()
+        for fixture in fixtures:
+            print(f"{fixture.home_team} vs {fixture.away_team}: {fixture.home_score} - {fixture.away_score}")
         print("Running cron job: updating tip results...")
         update_user_tip_stats()
-        print("Done.")
+        print("Scores updated, ✅.")
+        print(f"Results for current round:")
+        results = UserTipStats.query.filter_by(round_number=find_current_round()).all()
+        for result in results:
+            print(f"{result.user.username} got: {result.successful_tips}")
+        print("Schema check! ✅")
+        fixtures = get_free_nrl_fixtures()
+        print(json.dumps(fixtures[0], indent=2))
 
 if __name__ == "__main__":
     run()
