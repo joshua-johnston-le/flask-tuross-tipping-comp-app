f727deb7c1b662b7428ca96b609d8fe2076a00f0
joshua-johnston-le <joshua.johnston@luxuryescapes.com>
2025-05-22 12:23:05 +1000

    tipo

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(300), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=True),
+    phone_number = db.Column(db.String(20), nullable=True)
     avatar = db.Column(db.String(120), nullable=False, default="default.jpg")
     is_admin = db.Column(db.Boolean, default=False)
 
